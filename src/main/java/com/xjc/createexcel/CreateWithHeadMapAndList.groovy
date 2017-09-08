42bc20dd8fdec792143920298caef097015ebe0a
dachongxiaocong <ccyxxjc3@126.com>
2017-09-08 14:45:46 +0800

    v1.1

--- src/main/java/com/xjc/createexcel/CreateWithHeadMapAndList.groovy
+++ src/main/java/com/xjc/createexcel/CreateWithHeadMapAndList.groovy
@@ -7,15 +7,22 @@ import org.apache.poi.hssf.usermodel.HSSFSheet
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
 
 class CreateWithHeadMapAndList<T> {
+    //groovy支持在方法定义时设置默认属性值，LinkedHashMap可以保证放入数据的顺序，通过map设置标题与类中全局变量的对应关系和数据列顺序，List为需要插入的行对象，T为行对象类
     HSSFWorkbook create(LinkedHashMap<String,String> heads,List<T> objects,String sheetName = "default"){
+        //获取workbook对象
         HSSFWorkbook workbook = new HSSFWorkbook()
+        //获取sheet对象
         HSSFSheet sheet = workbook.createSheet(sheetName)
+        //获取标题行对象
         HSSFRow headRow = sheet.createRow(0)
+        //获取行对象插入工具类
         FillObjectIntoRow<T> fillObject = new FillObjectIntoRow<>()
         int headCellNum = 0
         int bodyRowNum = 1
+        //通过map获取这个list，这个list的作用是保证列数据顺序是用户在map中设置的顺序
         List<String> class_titles= new ArrayList<>()
 
+        //插入标题
         for (head in heads){
             String title = head.key
             String class_title = head.value
@@ -24,6 +31,7 @@ class CreateWithHeadMapAndList<T> {
             class_titles<<class_title
             headCellNum++
         }
+        //填入行数据
         for (object in objects){
             HSSFRow bodyRow = sheet.createRow(bodyRowNum)
             fillObject.fill(object,bodyRow,class_titles)
--- src/main/java/com/xjc/createexcel/Util/FillObjectIntoRow.groovy
+++ src/main/java/com/xjc/createexcel/Util/FillObjectIntoRow.groovy
@@ -4,17 +4,24 @@ import org.apache.poi.hssf.usermodel.HSSFCell
 import org.apache.poi.hssf.usermodel.HSSFRow
 
 import java.lang.reflect.Field
-
 class FillObjectIntoRow<T> {
+    //向行中填入数据，数据顺序由list决定
     void fill(T t,HSSFRow row,List<String> class_titles){
+        //获取行数据类对象的class对象
         Class<T> tClass = t.class
+        //通过反射获取所有全局属性
         Field[] fields = tClass.declaredFields
         int cellNum = 0
+        //遍历list，通过list的顺序决定行数据的顺序
         for (class_title in class_titles){
+            //遍历类中的属性
             fields.each {field ->
                 if (field.name.equals(class_title)){
+                    //groovy对抽象的简化，可以获得对象同名属性的值
                     String value = t."$field.name"
+                    //获取单元格对象
                     HSSFCell cell = row.createCell(cellNum)
+                    //将值填入单元格中
                     cell.setCellValue(value)
                 }
             }
